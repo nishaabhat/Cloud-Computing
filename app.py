8e1de0c293d7a36926e18feddab6303195fd35c5
Nisha Bhat <38810321+nishaabhat@users.noreply.github.com>
2022-04-10 18:47:06 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -9,45 +9,43 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 # Init db
 db = SQLAlchemy(app)
 # Init ma
 ma = Marshmallow(app)
 
-@app.route('/')
-def index():
-  return 'Welcome to clothing app'
-
 # Product Class/Model
 class Product(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(100), unique=True)
+  description = db.Column(db.String(200))
   price = db.Column(db.Float)
   qty = db.Column(db.Integer)
 
   def __init__(self, name, description, price, qty):
     self.name = name
+    self.description = description
     self.price = price
     self.qty = qty
 
 # Product Schema
 class ProductSchema(ma.Schema):
   class Meta:
-    fields = ('id', 'name', 'price', 'qty')
+    fields = ('id', 'name', 'description', 'price', 'qty')
 
 # Init schema
-product_schema = ProductSchema()
-products_schema = ProductSchema(many=True)
+product_schema = ProductSchema(strict=True)
+products_schema = ProductSchema(many=True, strict=True)
 
 # Create a Product
 @app.route('/product', methods=['POST'])
 def add_product():
   name = request.json['name']
+  description = request.json['description']
   price = request.json['price']
   qty = request.json['qty']
 
-  new_product = Product(name, price, qty)
+  new_product = Product(name, description, price, qty)
 
   db.session.add(new_product)
   db.session.commit()
@@ -59,7 +57,7 @@ def add_product():
 def get_products():
   all_products = Product.query.all()
   result = products_schema.dump(all_products)
-  return jsonify(result)
+  return jsonify(result.data)
 
 # Get Single Products
 @app.route('/product/<id>', methods=['GET'])
@@ -73,10 +71,12 @@ def update_product(id):
   product = Product.query.get(id)
 
   name = request.json['name']
+  description = request.json['description']
   price = request.json['price']
   qty = request.json['qty']
 
   product.name = name
+  product.description = description
   product.price = price
   product.qty = qty
 
