472046e35815340086df0c2fd31c2097ae18c5e9
Nisha Bhat <38810321+nishaabhat@users.noreply.github.com>
2022-04-11 04:02:44 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import hashlib
 # Init app
 app = Flask(__name__)
 app.secret_key = 'CC-GP12'
-basedir = os.path.abspath(os.path.dirname(__file__))
+#basedir = os.path.abspath(os.path.dirname(__file__))
 # Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Password123@database-1.cmdfiwgzwnsx.us-east-1.rds.amazonaws.com:5432/postgres'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
@@ -19,13 +19,15 @@ db = SQLAlchemy(app)
 # Init ma
 ma = Marshmallow(app)
 
-
+#Defining user
 class User(db.Model):
+    #declaring variables
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
     email = db.Column(db.String(80))
     userpassword = db.Column(db.String())
 
+    #constructor for table instance
     def __init__(self, username, email, userpassword):
       self.username = username
       self.email = email
@@ -40,6 +42,7 @@ class UserSchema(ma.Schema):
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
+#add new user
 @app.route('/adduser', methods=['POST'])
 def add_user():
   username = request.json['username']
@@ -48,40 +51,40 @@ def add_user():
   existing_user = User.query.filter_by(username=username).first()
   if existing_user:
     return {"message": "User already exists"}, 403
-
+  #hashing the password
   o_hash = hashlib.new('ripemd160')
   o_hash.update(userpassword.encode("utf-8"))
   userpassword = o_hash.hexdigest() 
   new_user = User(username, email, userpassword)
-
+  #writing to database
   db.session.add(new_user)
   db.session.commit()
+  return user_schema.jsonify(new_user), 200
 
-  return user_schema.jsonify(new_user)
-
-@app.route('/getuser', methods=['GET'])
+#method to get all users
+@app.route('/getusers', methods=['GET'])
 def get_user():
   all_users = User.query.all()
   result_users = users_schema.dump(all_users)
-  return jsonify(result_users)
+  return jsonify(result_users), 200
 
+#method to authenticate user based on hashed password
 @app.route('/authenticate', methods = ['POST'])
 def authenticate():
   username = request.json['username']
   userpassword = request.json['userpassword']
 
+  #hash the password received
   o_hash = hashlib.new('ripemd160')
   o_hash.update(userpassword.encode("utf-8"))
   fetchpassword = o_hash.hexdigest()
 
   user = User.query.filter_by(username = username).first()
-
-
+  #check if the password matches
   if user is not None and user.userpassword == fetchpassword:
-    text = 'Password matches. User '+username+' authenticated.'
+    return 'Password matches. User '+username+' authenticated.', 200
   else:
-    text = 'Password mismatch!!'
-  return text
+    return 'Username or password incorrect!!', 403
 
 # Product Class/Model
 class Product(db.Model):
@@ -91,6 +94,7 @@ class Product(db.Model):
   price = db.Column(db.Float)
   qty = db.Column(db.Integer)
 
+  #constructor for table instance
   def __init__(self, name, description, price, qty):
     self.name = name
     self.description = description
@@ -107,7 +111,7 @@ product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
 
 # Create a Product
-@app.route('/product', methods=['POST'])
+@app.route('/addproduct', methods=['POST'])
 def add_product():
   name = request.json['name']
   description = request.json['description']
@@ -123,20 +127,25 @@ def add_product():
   db.session.add(new_product)
   db.session.commit()
 
-  return product_schema.jsonify(new_product)
+  return product_schema.jsonify(new_product), 200
 
 # Get All Products
-@app.route('/product', methods=['GET'])
+@app.route('/getproduct', methods=['GET'])
 def get_products():
   all_products = Product.query.all()
+  if all_products is None:
+    return {"message": "No products available"}, 200
   result = products_schema.dump(all_products)
-  return jsonify(result)
+  return jsonify(result), 200
 
 # Get Single Products
-@app.route('/product/<id>', methods=['GET'])
+@app.route('/getproduct/<id>', methods=['GET'])
 def get_product(id):
   product = Product.query.get(id)
-  return product_schema.jsonify(product)
+  if product == None:
+    return {"message": "Product doesn't exist exists"}, 403
+  else:
+    return product_schema.jsonify(product), 200
 
 # Update a Product
 @app.route('/product/<id>', methods=['PUT'])
@@ -154,7 +163,7 @@ def update_product(id):
     product.price = price
     product.qty = qty
     db.session.commit()
-    return product_schema.jsonify(product)
+    return product_schema.jsonify(product), 200
 
 # Delete Product
 @app.route('/product/<id>', methods=['DELETE'])
